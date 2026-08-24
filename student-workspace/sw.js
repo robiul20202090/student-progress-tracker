9af0905085c7366f5db1586bb1a01d9b2b03118a
robiul20202090 <robiul20202090@gmail.com>
2026-08-24 12:57:29 +0000

    Add files via upload

--- student-workspace/sw.js
+++ student-workspace/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'scholastic-ledger-workspace-v10';
+const CACHE_NAME = 'scholastic-ledger-workspace-v12-guardian-preview';
 const APP_SHELL = ['./', './index.html', './manifest.json', './brand-logo.png'];
 
 self.addEventListener('install', (event) => {
